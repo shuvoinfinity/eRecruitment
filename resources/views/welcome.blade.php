79f24f28918d85fd5d0e471e915105591c840e84
shuvoinfinity <shuvo.ndc6011@gmail.com>
2016-01-19 23:16:35 +0600

    form started

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,82 @@
                 <div class="panel-body">
                     Your Application's Landing Page.
                 </div>
+                <div >
+                    {!! Form::open( array( 'class' => 'form')) !!}
+                    <div>
+                        <div class = "form-group col-md-6">
+
+                            {!! Form::label('Your Name') !!}
+                            {!! Form::text('name', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your name')) !!}
+
+                        </div>
+
+                        <div class = "form-group col-md-6">
+                            {!! Form::label('Your Email') !!}
+                            {!! Form::text('email', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your email Address')) !!}
+
+                        </div>
+
+                    </div>
+
+                    <div>
+                        <div class = "form-group col-md-6">
+
+                            {!! Form::label('Your Name') !!}
+                            {!! Form::text('name', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your name')) !!}
+
+                        </div>
+
+                        <div class = "form-group col-md-6">
+                            {!! Form::label('Your Email') !!}
+                            {!! Form::text('email', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your email Address')) !!}
+
+                        </div>
+                        
+                    </div>
+
+                    <div>
+                        <div class = "form-group col-md-6">
+
+                            {!! Form::label('Your Name') !!}
+                            {!! Form::text('name', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your name')) !!}
+
+                        </div>
+
+                        <div class = "form-group col-md-6">
+                            {!! Form::label('Your Email') !!}
+                            {!! Form::text('email', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your email Address')) !!}
+
+                        </div>
+                        
+                    </div>
+
+
+                    <div class = "form-group">
+                        {!! Form::label('Your Name') !!}
+                        {!! Form::text('name', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your name')) !!}
+
+                    </div>
+
+                    <div class = "form-group">
+                        {!! Form::label('Your Email') !!}
+                        {!! Form::text('email', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your email Address')) !!}
+
+                    </div>
+
+                    <div class = "form-group">
+                        {!! Form::label('Your Message') !!}
+                        {!! Form::textarea('message', null, array('required', 'class' => 'form-control', 'placeholder' => 'Your mesasge')) !!}
+
+                    </div>
+
+                    <div class = "form-group">
+                        {!! Form::submit('Contact Us!', array('class' => 'btn btn-primary')) !!}
+
+                    </div>
+
+                    {!! Form::close() !!}
+                </div>
             </div>
         </div>
     </div>
